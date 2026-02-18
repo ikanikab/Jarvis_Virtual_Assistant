a15f8d9eb4bbc4941de2e7e3363f365351bab1a7
ikanikab <ikanikabhardwaj18@gmail.com>
2026-02-19 03:44:14 +0530

    api

--- test.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAIAySqvZmPZRZ7ry-U5MCNlSJR0vGUWkg")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("say hello")
-print(response.text)
